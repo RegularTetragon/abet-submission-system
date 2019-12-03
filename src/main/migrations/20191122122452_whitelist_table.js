95cac06c75722ae860063c0d2da1662cb4db7229
Gladmirth <thelibertyboys1@gmail.com>
2019-12-03 13:29:01 -0500

    Create new database migration. Add table whitelist, Add column to users Permission.

--- src/main/migrations/20191122122452_whitelist_table.js
+++ src/main/migrations/20191122122452_whitelist_table.js
@@ -1,19 +1,22 @@
 
-exports.up = function(knex, Promise)
+exports.up = function(knex)
 {
-    return Promise.all([
-    knex.schema.createTable('whitelist', function(table)
-    {
-        table.increments('id')
-				.primary()
-        table.string('email', 50).unique();
-    }),
-    ]);
+    return knex.schema
+        .createTable('whitelist', function(table)
+            {
+                table.increments('id')
+    				.primary()
+                table.string('email', 50).unique();
+            })
+            .alterTable('users', function(table)
+            {
+                table.integer('permission_level').notNull().defaultTo(1);
+                // restrict from 0-3
+            })
 };
 
-exports.down = function(knex, Promise)
+exports.down = function(knex)
 {
-    return Promise.all([
-    knex.schema.dropTable('whitelist'),
-    ]);
+    return knex.schema.dropTableIfExists('whitelist')
 };
+
